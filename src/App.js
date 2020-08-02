99b9c94044accee1d92b0bfdfb84fe6e1f438825
melvin-Code <melvingonzalez470@gmail.com>
2020-08-01 21:38:50 -0400

    time

--- src/App.js
+++ src/App.js
@@ -44,15 +44,7 @@ rendering=()=>{
 
 
 }
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
-  // };
-  // handleChange = (e) => {
-  //   this.setState({
-  //     [e.target.name]: e.target.value,
-  //   });
-  //   console.log("yes");
-  // };
+  /
   render() {
     // console.log(this.state)
     return (
